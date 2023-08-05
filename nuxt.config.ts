a92caa530f70b24230f565b89fcafa6866bda585
Qin Guan <qinguan20040914@gmail.com>
2023-08-05 23:50:45 +0800

    feat: pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxthq/ui'],
+  nitro: {
+    prerender: {
+      routes: ['/'],
+    },
+  },
+  modules: [
+    '@vite-pwa/nuxt',
+    '@nuxthq/ui',
+  ],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Cash Register',
+      short_name: 'CashRegister',
+      theme_color: '#ffffff',
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      periodicSyncForUpdates: 20,
+    },
+  },
 })
