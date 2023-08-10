bd3762921a8d18e4b5913fec45c526e80b735303
Qin Guan <qinguan20040914@gmail.com>
2023-08-11 00:16:33 +0800

    fix: explicit use eventHandler

--- server/api/healthz.get.ts
+++ server/api/healthz.get.ts
@@ -1,5 +1,5 @@
 import { users } from '~/server/db/schema'
 
-export default defineLazyEventHandler(() => async (event) => {
+export default defineLazyEventHandler(() => eventHandler(async (event) => {
   return await event.context.database.select().from(users).all()
-})
+}))
