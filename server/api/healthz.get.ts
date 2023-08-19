3c85b4a0535082ccf214fa93c954cda50f95d312
Julian Teh <mail@tehj.io>
2023-08-19 15:44:00 +0800

    Changing function name to conform to Nuxt documentation

--- server/api/healthz.get.ts
+++ server/api/healthz.get.ts
@@ -1,5 +1,5 @@
 import { users } from '~/server/db/schema'
 
-export default defineLazyEventHandler(() => eventHandler(async (event) => {
+export default defineEventHandler(() => eventHandler(async (event) => {
   return await event.context.database.select().from(users).all()
 }))
