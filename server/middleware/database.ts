7db7b4cc854ab1198f2638c3f84e1ff881e197c0
Qin Guan <qinguan20040914@gmail.com>
2023-08-21 06:50:09 +0800

    wip

--- server/middleware/database.ts
+++ server/middleware/database.ts
@@ -5,8 +5,6 @@ import type { BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
 import { drizzle as drizzleSqlite } from 'drizzle-orm/better-sqlite3'
 import Database from 'better-sqlite3'
 
-import { isCI, isDevelopment } from 'std-env'
-
 declare module 'h3' {
   interface H3EventContext {
     database: DrizzleD1Database | BetterSQLite3Database
@@ -14,12 +12,12 @@ declare module 'h3' {
 }
 
 export default defineEventHandler((event) => {
-  if (isDevelopment || isCI) {
+  if (event.context.cloudflare) {
+    event.context.database = drizzleD1(event.context.cloudflare.env.DATABASE)
+  }
+  else {
     // The database instance is recreated on every request to simulate the actual Cloudflare Workers environment, this may or may not be a bad idea
     const sqlite = new Database(useRuntimeConfig().dev.sqliteFileName)
     event.context.database = drizzleSqlite(sqlite)
-  }
-  else {
-    event.context.database = drizzleD1(event.context.cloudflare.env.DATABASE)
   }
 })
