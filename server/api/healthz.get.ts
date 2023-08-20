113182f0a8eb2ea47c6ffa116c62281ec1e36330
Julian Teh <mail@tehj.io>
2023-08-20 21:16:38 +0800

    Just define event handler instead of defining event handler that defines event handler

--- server/api/healthz.get.ts
+++ server/api/healthz.get.ts
@@ -1,5 +1,5 @@
 import { users } from '~/server/db/schema'
 
-export default defineEventHandler(() => eventHandler(async (event) => {
+export default defineEventHandler(async (event) => {
   return await event.context.database.select().from(users).all()
-}))
+});
