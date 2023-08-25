2e64215384e4924dea4c904db82590ab4a268008
Qin Guan <qinguan20040914@gmail.com>
2023-08-25 17:04:54 +0800

    ?

--- server/plugins/database.ts
+++ server/plugins/database.ts
@@ -5,8 +5,8 @@ import { isCI, isDevelopment } from 'std-env'
 import Database from 'better-sqlite3'
 
 export default defineNitroPlugin(async () => {
-  if (isDevelopment || isCI) { // Migration should not run in production
-    const db = betterSqlite3Drizzle(new Database(useRuntimeConfig().dev.sqliteFileName))
-    betterSqlite3Migrate(db, { migrationsFolder: './server/drizzle' })
-  }
+  // if (isDevelopment || isCI) { // Migration should not run in production
+  //   const db = betterSqlite3Drizzle(new Database(useRuntimeConfig().dev.sqliteFileName))
+  //   betterSqlite3Migrate(db, { migrationsFolder: './server/drizzle' })
+  // }
 })
